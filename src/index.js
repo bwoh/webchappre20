8646aacef74a5d24a39da15a84f794a295338903
BWOh <bwohkit@gmail.com>
2022-02-10 04:28:20 +0900

    🎉 begin project

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("성공");
+const mechanic = [
+  { id: 1, name: "건담", model: "RX-78-2" },
+  { id: 2, name: "건탱크", model: "RX-75" },
+  { id: 3, name: "자쿠II", model: "MS-06F" }
+];
+
+app.get("/mechanic", (req, res) => {
+  res.json(mechanic);
 });
 
 app.listen(port, () => {
